e6c31283fa133900e7434f2b0497770c8cb9d8aa
JCampano <>
2018-02-24 21:19:27 +0100

    Alta de Empleados funcional (con $.ajax).

--- Servicio-tecnico-moviles-master/personal/altaempleado.js
+++ Servicio-tecnico-moviles-master/personal/altaempleado.js
@@ -153,5 +153,37 @@ function validarAltaEmpleado(){
 		alert(sErrores);
 	else{
 		//altaEmpleado
-	}		
+         $.ajax({
+            url: "personal/altaempleado.php",
+            async: true,
+            cache: false,
+            method: "POST",
+            dataType: "json",
+            data: $("#frmAltaEmpleado").serializeArray(),
+            //  beforeSend: prepararDatosEnvio,
+            complete: respuestaAltaEmpleado
+        });
+	   }
+}
+
+function respuestaAltaEmpleado(jqXHR, sStatus)
+{
+    var oDatosDevueltos = JSON.parse(jqXHR.responseText);
+    //var oDatosDevueltos = jqXHR.responseJSON;
+
+    if (sStatus == "success") {
+        // oDatosDevueltos[0]  --- si hay o no error
+        if (oDatosDevueltos[0] == false) {
+            // Mensaje
+            alert(oDatosDevueltos[1]);
+
+            // Como ha ido bien cierro el formulario
+            $("#frmAltaEmpleado").hide("normal").get(0).reset();
+
+        } else {
+            alert(oDatosDevueltos[1]);
+        }
+    } else {
+        alert("Error del servidor: " + sStatus);
+    }
 }
---
+++ Servicio-tecnico-moviles-master/personal/altaempleado.php
@@ -0,0 +1,62 @@
+<?php
+
+// Va a devolver una respuesta JSON que no se debe cachear
+header('Content-Type: application/json');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+
+
+$servidor  = "localhost";
+$basedatos = "mobilexpressdb";
+$usuario   = "root";
+$password  = "";
+
+$nifEmpleado=$_REQUEST['nifEmpleado'];
+$nombreEmpleado=$_REQUEST['nombreEmpleado'];
+$apellidosEmpleado=$_REQUEST['apellidosEmpleado'];
+$direccionEmpleado=$_REQUEST['direccionEmpleado'];
+$telefonoEmpleado=$_REQUEST['telefonoEmpleado'];
+
+
+// Abrir conexion con la BD
+$conexion = mysqli_connect($servidor, $usuario, $password, $basedatos);
+if($conexion->connect_error){
+        die("Conexión fallida: ".$conexion->connect_error);
+    }
+$conexion ->set_charset("utf8");//asi es el caracter utf8 si es msqli
+
+$sql = "SELECT * FROM persona WHERE dni_o_cif='".$nifEmpleado."' and tipo='Empleado' ";
+
+
+if ($res = $conexion->query($sql)){
+    $contador = $res->num_rows;
+}
+
+if($contador>0)
+{
+	$mensaje= 'Ya existe ese empleado';
+	$error = true;
+
+}
+else
+{
+	$sql = "INSERT INTO persona(dni_o_cif, tipo, nombre, apellidos, telefono, direccion) VALUES ('$nifEmpleado','Cliente', '$nombreEmpleado', '$apellidosEmpleado', '$telefonoEmpleado', '$direccionEmpleado')";
+
+	if($conexion->query($sql) === TRUE){
+	        $mensaje = "Alta de Empleado correcta";
+		    $error = FALSE;
+	    }
+	    else {
+		    $mensaje = "Error: ".$sql." ".$conn->error;
+		    $error = TRUE;
+		}
+
+}
+
+$respuesta = array($error,$mensaje);
+
+echo json_encode($respuesta);
+
+mysqli_close($conexion);
+
+?>
